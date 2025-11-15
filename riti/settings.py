2db16fb278bd2b010d5392353d2206bbdb051b3c
kothasathwik07 <kotha.sathwik07@gmail.com>
2025-11-16 00:28:43 +0530

    Changes for cloudinary

--- riti/settings.py
+++ riti/settings.py
@@ -177,7 +177,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 import os
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cloudinary Media Storage
